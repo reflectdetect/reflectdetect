2400b98d04ba35af521fa4294e87313c6733a2d7
Luca Francis <lucajoshua.francis@stud.uni-goettingen.de>
2024-05-12 13:45:46 +0200

    save metrics from pipeline runs

--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import os
+from datetime import datetime
 
 import shapely.geometry as sg
 
@@ -11,18 +13,27 @@ def test_pytest():
 
 
 def test_main_performance():
-    results = run_detection("../data/example/YOLO_OBB_Dataset")
+    dataset_path = "../data/example/YOLO_OBB_Dataset"
+    results = run_detection(dataset_path)
     assert len(results) == 5
+    overall_metrics = {"dataset": dataset_path, }
     for image_path, transformed_image_path, extraction_path, detection_path in results:
-        get_metrics(image_path, transformed_image_path, extraction_path, detection_path)
+        overall_metrics[str(get_id_from_path(image_path))] = get_metrics(image_path, transformed_image_path,
+                                                                         extraction_path, detection_path)
+    save_metrics(overall_metrics)
+
+
+def get_id_from_path(image_path):
+    return int(str(image_path).split("/")[0].split("_")[-1].split(".")[0])
 
 
 def get_metrics(image_path, transformed_image_path, extraction_path, detection_path):
     with open("../data/example/YOLO_OBB_Dataset/annotations/seq1.csv") as annotations_file:
         reader = csv.DictReader(annotations_file, delimiter=',')
         annotations = [row for row in reader]
-        identifier = int(str(image_path).split("/")[0].split("_")[-1].split(".")[0])
-        ground_truth_annotations = [row for row in annotations if int(row["id"]) == identifier - 1]
+        identifier = get_id_from_path(image_path)
+        ground_truth_annotations = [row for row in annotations if int(row["id"]) == identifier]
+        metrics = []
         # Detection_metric
         with open(detection_path) as f:
             detections = json.load(f)
@@ -32,7 +43,8 @@ def get_metrics(image_path, transformed_image_path, extraction_path, detection_p
                     gt_list = list(zip(gt_list, gt_list[1:]))[::2]
                     detection_list = detection[2:]
                     detection_list = list(zip(detection_list, detection_list[1:]))[::2]
-                    print(calculate_overlap_metrics(detection_list, gt_list))
+                    metrics.append(calculate_overlap_metrics(detection_list, gt_list))
+        return metrics
 
 
 def calculate_overlap_metrics(rect1, rect2):
@@ -70,3 +82,26 @@ def calculate_overlap_metrics(rect1, rect2):
     }
 
     return metrics
+
+
+def save_metrics(metrics, filename_prefix="../metrics/metrics"):
+    """
+    Saves overlap metrics to a timestamped CSV file.
+
+    Args:
+        metrics: A dictionary of metrics (output of calculate_overlap_metrics).
+        filename_prefix (optional): A prefix for the filename. Defaults to "overlap_metrics".
+    """
+
+    # Check if running in GitHub Actions
+    is_github_actions = os.getenv("GITHUB_ACTIONS") == "true"
+
+    # Generate timestamped filename with postfix if applicable
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"{filename_prefix}_{timestamp}{'_github' if is_github_actions else ''}.json"
+
+    # Write to CSV file
+    with open(filename, "w") as jsonfile:
+        json.dump(metrics, jsonfile, indent=4)  # Use indent for pretty formatting
+
+    print(f"Metrics saved to {filename}")
